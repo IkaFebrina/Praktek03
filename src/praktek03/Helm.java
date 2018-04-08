170ed0bb7a6a584d247f726cec83495cf9aecb65
IkaFebrina <32430894+IkaFebrina@users.noreply.github.com>
2018-04-08 16:00:47 +0700

    Membuat Getter and Setter

--- src/praktek03/Helm.java
+++ src/praktek03/Helm.java
@@ -11,4 +11,38 @@ public class Helm {
                 "Jenis_Kaca \t: "+Jenis_Kaca+"\n"+
                 "Harga \t: "+Harga);
     }
+
+    public String getMerk() {
+        return Merk;
+    }
+
+    public void setMerk(String Merk) {
+        this.Merk = Merk;
+    }
+
+    public String getWarna() {
+        return Warna;
+    }
+
+    public void setWarna(String Warna) {
+        this.Warna = Warna;
+    }
+
+    public String getJenis_Kaca() {
+        return Jenis_Kaca;
+    }
+
+    public void setJenis_Kaca(String Jenis_Kaca) {
+        this.Jenis_Kaca = Jenis_Kaca;
+    }
+
+    public double getHarga() {
+        return Harga;
+    }
+
+    public void setHarga(double Harga) {
+        this.Harga = Harga;
+    }
+    
+    
 }
