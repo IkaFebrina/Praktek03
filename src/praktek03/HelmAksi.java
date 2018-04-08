a0cf40231f1997786ec57f62f24d211d347e882e
IkaFebrina <32430894+IkaFebrina@users.noreply.github.com>
2018-04-08 15:57:18 +0700

    Instance, Membuat Attribut dan panggil Method

--- src/praktek03/HelmAksi.java
+++ src/praktek03/HelmAksi.java
@@ -1,6 +1,26 @@
 package praktek03;
 public class HelmAksi {
     public static void main(String[] args) {
+        Helm Dodo = new Helm();
+        Dodo.Merk="GM";
+        Dodo.Warna="Dark Blue";
+        Dodo.Jenis_Kaca="Kaca Bening";
+        Dodo.Harga=250000;
         
+        Helm Dodi = new Helm();
+        Dodi.Merk="KYT";
+        Dodi.Warna="Black";
+        Dodi.Jenis_Kaca="Kaca Gelap";
+        Dodi.Harga=300000;
+        
+        Helm Dudu = new Helm();
+        Dudu.Merk ="NKH";
+        Dudu.Warna="Black";
+        Dudu.Jenis_Kaca="Kaca Bening + Kaca Gelap (double)";
+        Dudu.Harga=450000;
+        
+        Dodo.cetakInfo();
+        Dodi.cetakInfo();
+        Dudu.cetakInfo();
     }
 }
