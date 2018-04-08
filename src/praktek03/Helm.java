80b8c89ada1addb52e1eb4b5a13cc5970fa8574a
IkaFebrina <32430894+IkaFebrina@users.noreply.github.com>
2018-04-08 14:44:15 +0700

    Membuat Method cetakInfo()

--- src/praktek03/Helm.java
+++ src/praktek03/Helm.java
@@ -4,4 +4,11 @@ public class Helm {
     String Warna;
     String Jenis_Kaca;
     double Harga;
+    
+    void cetakInfo(){
+        System.out.println("Merk \t: "+Merk+"\n"+
+                "Warna \t: "+Warna+"\n"+
+                "Jenis_Kaca \t: "+Jenis_Kaca+"\n"+
+                "Harga \t: "+Harga);
+    }
 }
